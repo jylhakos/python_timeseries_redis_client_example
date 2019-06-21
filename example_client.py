80d1831c893415759179881bc2dbdb803f2b1f69
Juha Jylhäkoski <51870080+jylhakos@users.noreply.github.com>
2019-06-21 10:43:20 +0800

    Update expire and delete key

--- example_client.py
+++ example_client.py
@@ -14,20 +14,66 @@ ts.flushdb()
 
 key = 'temperature'
 
+def create(key):
+
+	print('\n Create new time series: %s' % str(key))
+
+	ts.create(key,retentionSecs=30,labels={'sensor_id' : 2,'area_id' : 32})
+
+	print('')
+
+def store(key, interval):
+
+	print("\n Append new value to time series:\n")
+
+	begin_time = int(time.time())
+
+	for i in range(interval):
+
+		timestamp = int(time.time())
+
+		value = round(random.uniform(0.0,100.0),2)
+
+		timestamp_strftime = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+		sys.stdout.write(' %s : %.2f \n' % (timestamp_strftime, value))
+
+		sys.stdout.flush()
+
+		ts.add(key,timestamp,value,retentionSecs=30, labels={'sensor_id' : 2,'area_id' : 32})
+
+		time.sleep(1)
+
+	end_time = int(time.time()-1)
+
+	return (begin_time, end_time)
+
 def query(key, begin_time, end_time):
+
+	begin_time_datetime = datetime.datetime.fromtimestamp(begin_time).strftime('%Y-%m-%d %H:%M:%S')
+
+	end_time_datetime = datetime.datetime.fromtimestamp(end_time).strftime('%Y-%m-%d %H:%M:%S')
+
+	print("\n Query time series in range:\n\n %s to %s \n" % (begin_time_datetime, end_time_datetime))
+
 	try:
 		for record in ts.range(key,begin_time, end_time,bucketSizeSeconds=1):
 
 			timestamp = datetime.datetime.fromtimestamp(record[0]).strftime('%Y-%m-%d %H:%M:%S')
 			
-			data = round(float(record[1]),2)
+			value = round(float(record[1]),2)
 
-			print(' %s : %.2f ' % (timestamp, data))
+			print(' %s : %.2f ' % (timestamp,value))
 
 	except Exception as e:
 		print("\n Error: %s" % e)
 
+	print('')
+
 def print_info():
+
+	print('\n Query time series info:\n')
+
 	for key in ts.keys('*'):
 		print(' key=%s' % (key.decode('utf8')))
 		info = ts.info(key)
@@ -36,49 +82,64 @@ def print_info():
 		area = info.labels['area_id']
 		print(" area_id=%s " % str(area))
 
-begin_time = int(time.time())
+	print('')
 
-ts.create(key,retentionSecs=30,labels={'sensor_id' : 2,'area_id' : 32})
+def print_loop(loops):
 
-begin_time_datetime = datetime.datetime.fromtimestamp(begin_time).strftime('%Y-%m-%d %H:%M:%S')
+	for i in range(loops):
 
-print("\n Quick insert of the data by start time:\n")
+		if i == 0:
+			sys.stdout.write(' ')
 
-for i in range(10):
+		sys.stdout.write('.')
+		sys.stdout.flush()
+		time.sleep(1)
 
-	data = round(random.uniform(0.0,100.0),2)
+	print('')
 
-	timestamp = int(time.time())
+create(key)
 
-	timestamp_strftime = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+interval = 10
 
-	sys.stdout.write(' %s : %.2f \n' % (timestamp_strftime, data))
-
-	sys.stdout.flush()
-
-	ts.add(key,timestamp,data,retentionSecs=30, labels={'sensor_id' : 2,'area_id' : 32})
-
-	time.sleep(1)
-
-print('')
-
-end_time = int(time.time())
-
-end_time_datetime = datetime.datetime.fromtimestamp(end_time).strftime('%Y-%m-%d %H:%M:%S')
+begin_time, end_time = store(key,interval)
 
 time.sleep(1)
 
-print("\n Query the data for a time range:\n\n %s to %s \n" % (begin_time_datetime, end_time_datetime))
-
 query(key,begin_time,end_time)
 
-print('')
+query(key,begin_time+4,end_time-5)
 
 print_info()
 
-print('')
+print('\n Set expire key: %s' % str(key))
+
+ts.expire(key, (30))
+
+loops = 30
+
+print_loop(loops)
+
+query(key,begin_time,end_time)
+
+time.sleep(1)
+
+interval = 1
+
+create(key)
+
+begin_time, end_time = store(key,interval)
+
+time.sleep(1)
+
+query(key,begin_time,end_time)
+
+time.sleep(1)
+
+print('\n Delete key: %s' % str(key))
 
 ts.delete(key)
+
+time.sleep(1)
 
 query(key,begin_time,end_time)
 
